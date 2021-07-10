e350a5d7ee4cd7d2d96c3919353b24cc6d54d6b9
Rizky Tanjung <kimsohyuncans@gmail.com>
2021-07-10 18:04:39 +0700

    Read data from database MySQL

--- app.js
+++ app.js
@@ -2,14 +2,45 @@
 const express = require("express");
 const app = express();
 const port = 9001;
-// Menggunakan ejs
 
+// Menggunakan ejs
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(express.static("stylesheet"));
 
-app.get("/", (req, res) => {
-  res.render("index");
+const mysql = require("mysql");
+const myconnection = require("express-myconnection");
+
+// connection to database MySQL
+const dbConfig = {
+  host: "localhost",
+  user: "root",
+  password: "",
+  port: "3306",
+  database: "db_warteg",
+};
+// End connection to database MySQL
+
+// Cara menggunakan koneksi antara database dengan server
+app.use(myconnection(mysql, dbConfig, "pool"));
+// End Cara menggunakan koneksi antara database dengan server
+
+// app.get("/", (req, res) => {
+//   res.render("index");
+// });
+
+app.get("/", (req, res, next) => {
+  // res.render("Book");
+  req.getConnection(function (err, conn) {
+    if (err) throw err;
+    conn.query(
+      "SELECT tb_food.*, tb_categori.name AS category_name FROM tb_food INNER JOIN tb_categori ON (tb_food.categori_id = tb_categori.id) WHERE tb_categori.name='Makanan Kering'",
+      function (err, rows) {
+        if (err) throw err;
+        res.render("index", { data: rows });
+      }
+    );
+  });
 });
 
 app.get("/add-category", (req, res) => {
